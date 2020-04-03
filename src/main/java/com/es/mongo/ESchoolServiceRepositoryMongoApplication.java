d10582d6dce2f0143a95acb2ef35beb087f994fe
weixx3@126.com <weixiaoxing@meishubao.com>
2020-04-03 20:01:18 +0800

    init

---
+++ src/main/java/com/es/mongo/ESchoolServiceRepositoryMongoApplication.java
@@ -0,0 +1,13 @@
+package com.es.mongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ESchoolServiceRepositoryMongoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ESchoolServiceRepositoryMongoApplication.class, args);
+    }
+
+}
